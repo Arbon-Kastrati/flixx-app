705eb63582751f592bbe408105d3b1e2d00460a1
Arbon Kastrati <arbon_kastrati@outlook.com>
2026-04-26 00:11:20 +0200

    Remove static content from shows and add dynamic generation of content for them

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
     currentPage: window.location.pathname,
 };
 const popularMoviesContainer = document.getElementById('popular-movies');
+const popularShowsContainer = document.getElementById('popular-shows');
 const API_URL = 'https://api.themoviedb.org/3';
 
 async function displayPopularMovies() {
@@ -24,6 +25,30 @@ async function displayPopularMovies() {
                 </p>
             </div>`;
         popularMoviesContainer.appendChild(div);
+    });
+}
+
+async function displayPopularShows() {
+    const { results } = await fetchAPIData('tv/popular');
+    results.forEach((show) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+            ${
+                show.poster_path
+                    ? `<img src="https://image.tmdb.org/t/p/w500/${show.poster_path}" class="card-img-top" alt="${show.original_name}"/>`
+                    : '<img src="images/no-image.jpg" class="card-img-top" alt="Show Title" />'
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.original_name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air date: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+        popularShowsContainer.appendChild(div);
     });
 }
 
@@ -59,12 +84,10 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
-            highlightActiveLink('movies');
             displayPopularMovies();
             break;
         case '/shows.html':
-            highlightActiveLink('shows');
-
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('movie details');
